356e256fa33e2b7377fa08ef48be7c9b88bd597f
River Phillips <riverphillips1@gmail.com>
2018-08-29 21:30:21 +0100

    Add get method to cities controller

---
+++ CintyInfo.API/CitiesDataStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CintyInfo.API.Models;
+
+namespace CintyInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id=1,
+                    Name="London",
+                    Description = "Capital city of the UK"
+                },
+                new CityDto()
+                {
+                    Id=2,
+                    Name="New York City",
+                    Description = "The Big Apple"
+                },
+                new CityDto()
+                {
+                    Id=3,
+                    Name="Bruges",
+                    Description = "Good Beer and Bikes"
+                }
+            };
+        }
+    }
+}
---
+++ CintyInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CintyInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+    }
+}
